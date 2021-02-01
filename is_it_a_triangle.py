dc62c3967ea6e0c8beb8158d3f1a8cf65f098d01
Hmntkr <hmntkumr024@gmail.com>
2021-02-01 10:23:56 +0530

    another program

---
+++ is_it_a_triangle.py
@@ -0,0 +1,28 @@
+﻿def isItATriangle(a, b, c):
+    if a + b <= c:
+        return False
+    if b + c <= a:
+        return False
+    if c + a <= b:
+        return False
+    return True
+
+print(isItATriangle(1, 1, 1))
+print(isItATriangle(1, 1, 3))
+
+
+def isItATriangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+print(isItATriangle(1, 1, 1))
+print(isItATriangle(1, 1, 3))
+
+
+def isItATriangle(a, b, c):
+    if a + b <= c or b + c <= a or \
+    c + a <= b:
+        return False
+    return True
+
+print(isItATriangle(1, 1, 1))
+print(isItATriangle(1, 1, 3))
